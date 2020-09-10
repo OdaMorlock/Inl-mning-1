4ab76e465c8ca131139336ee40a1dc49af3e8a87
Marcus <Marcus@DATOR>
2020-09-10 15:27:11 +0200

    Add project files.

---
+++ Temeprature/Worker.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+namespace Temperature
+{
+    public class Worker : BackgroundService 
+    {
+        private readonly ILogger<Worker> _logger;
+
+        
+        
+
+
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+           
+            _logger.LogInformation("The service has been started.");
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            
+            _logger.LogInformation("The service has been stopped.");
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                int temperature;
+                Random random = new Random();
+                temperature = random.Next(1, 1000);
+
+                 
+
+                try
+                {
+                    if (temperature <= 750)
+                        _logger.LogInformation($"The Temperature is within normal peramatures {temperature}C");
+                    else
+                        _logger.LogInformation($"The Temeprature is too high {temperature}C");
+                }
+                catch (Exception)
+                {
+
+                    
+                }
+
+                await Task.Delay(60*1000, stoppingToken);
+            }
+        }
+    }
+}
